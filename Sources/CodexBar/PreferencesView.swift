4285efb9759dac0913442b080662ae9481746637
Rajiv <millord237@gmail.com>
2025-11-25 12:35:34 +0100

    Reduce prefs window height

--- Sources/CodexBar/PreferencesView.swift
+++ Sources/CodexBar/PreferencesView.swift
@@ -8,7 +8,7 @@ enum PreferencesTab: String, Hashable {
     case debug
 
     static let windowWidth: CGFloat = 424
-    static let windowHeight: CGFloat = 456
+    static let windowHeight: CGFloat = 410
 
     var preferredHeight: CGFloat { PreferencesTab.windowHeight }
 }
