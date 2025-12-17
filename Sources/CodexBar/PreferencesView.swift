ca15b54bea23bea9b84a831b3eed5b6b3945ee8f
Rajiv <millord237@gmail.com>
2025-12-17 20:10:08 +0100

    style: adjust prefs height

--- Sources/CodexBar/PreferencesView.swift
+++ Sources/CodexBar/PreferencesView.swift
@@ -8,7 +8,7 @@ enum PreferencesTab: String, Hashable {
     case debug
 
     static let windowWidth: CGFloat = 424
-    static let windowHeight: CGFloat = 498
+    static let windowHeight: CGFloat = 480
 
     var preferredHeight: CGFloat { PreferencesTab.windowHeight }
 }
