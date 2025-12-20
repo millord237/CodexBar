e590488fe839ba0479990fd45c3a2f95eebe2eb7
Rajiv <millord237@gmail.com>
2025-12-20 04:09:11 +0100

    fix provider switcher icon inset

--- Sources/CodexBar/StatusItemController+Menu.swift
+++ Sources/CodexBar/StatusItemController+Menu.swift
@@ -457,7 +457,7 @@ private final class ProviderSwitcherView: NSView {
                 target: self,
                 action: #selector(self.handleSelection(_:)))
             button.tag = index
-            button.image = segment.image
+            button.image = Self.paddedImage(segment.image, leading: 3)
             button.imagePosition = .imageLeading
             button.bezelStyle = .regularSquare
             button.isBordered = false
@@ -505,6 +505,21 @@ private final class ProviderSwitcherView: NSView {
             button.contentTintColor = isSelected ? self.selectedTextColor : self.unselectedTextColor
             button.layer?.backgroundColor = isSelected ? self.selectedBackground : self.unselectedBackground
         }
+    }
+
+    private static func paddedImage(_ image: NSImage, leading: CGFloat) -> NSImage {
+        let size = NSSize(width: image.size.width + leading, height: image.size.height)
+        let newImage = NSImage(size: size)
+        newImage.lockFocus()
+        let y = (size.height - image.size.height) / 2
+        image.draw(
+            at: NSPoint(x: leading, y: y),
+            from: NSRect(origin: .zero, size: image.size),
+            operation: .sourceOver,
+            fraction: 1.0)
+        newImage.unlockFocus()
+        newImage.isTemplate = image.isTemplate
+        return newImage
     }
 
     private static func switcherTitle(for provider: UsageProvider) -> String {
