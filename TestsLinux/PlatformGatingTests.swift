adbb8e3479175876f81e40a73e53e8303494be1f
Rajiv <millord237@gmail.com>
2025-12-25 17:09:17 +0100

    test: fix linux gating assertions

--- TestsLinux/PlatformGatingTests.swift
+++ TestsLinux/PlatformGatingTests.swift
@@ -6,8 +6,13 @@ struct PlatformGatingTests {
     @Test
     func claudeWebFetcher_isNotSupportedOnLinux() async {
         #if os(Linux)
-        await #expect(throws: ClaudeWebAPIFetcher.FetchError.notSupportedOnThisPlatform) {
+        let error = #expect(throws: ClaudeWebAPIFetcher.FetchError.self) {
             _ = try await ClaudeWebAPIFetcher.fetchUsage()
+        }
+        if let error, case .notSupportedOnThisPlatform = error {
+            #expect(true)
+        } else {
+            #expect(false)
         }
         #else
         #expect(true)
@@ -26,12 +31,16 @@ struct PlatformGatingTests {
     @Test
     func claudeWebFetcher_sessionKeyInfo_throwsOnLinux() {
         #if os(Linux)
-        #expect(throws: ClaudeWebAPIFetcher.FetchError.notSupportedOnThisPlatform) {
+        let error = #expect(throws: ClaudeWebAPIFetcher.FetchError.self) {
             _ = try ClaudeWebAPIFetcher.sessionKeyInfo()
+        }
+        if let error, case .notSupportedOnThisPlatform = error {
+            #expect(true)
+        } else {
+            #expect(false)
         }
         #else
         #expect(true)
         #endif
     }
 }
-
